9c32ac72eccf73db7fcc4273b6ded93572f6cc7e
Pete Hodgson <git@thepete.net>
2013-02-23 00:21:01 -0800

    add some specs for crumb rendering

--- public/specs/the_spec.js
+++ public/specs/the_spec.js
@@ -27,3 +27,40 @@ describe('Congo.ItemView', function(){
   });
 });
 
+
+
+describe( 'Congo.BreadcrumbView', function(){
+  
+  // stop the initialize function from whining
+  Congo.router = { on: function(){} }
+
+  describe( '.renderDatabase', function(){
+    var $el, db;
+
+    beforeEach(function(){
+      $el = $('<div>');
+      db = 'some database name';
+
+      var view = new Congo.BreadcrumbView({el:$el});
+      view.renderDatabase( db );
+    });
+
+    it( 'renders two crumbs in total', function(){
+      expect( $el.find("li h3").length ).toBe(2)
+    });
+    it( 'renders the right crumb text', function(){
+      expect( $el ).toHaveText( 'DATABASES/some database name');
+    });
+
+    it( 'renders the first crumb as a link to home', function(){
+      var $firstCrumb = $el.find("li h3").eq(0);
+      expect( $firstCrumb ).toContain('a#navIndex');
+      expect( $firstCrumb ).toHaveText('DATABASES/');
+    });
+
+    it( 'renders the second crumb as a plain DB name', function(){
+      var $secondCrumb = $el.find("li h3").eq(1);
+      expect( $secondCrumb ).toHaveText(db);
+    });
+  });
+});
