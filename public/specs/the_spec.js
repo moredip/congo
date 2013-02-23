ce90d7e4ff49ca0ab1de9db501ee16b0e7dbf838
Pete Hodgson <git@thepete.net>
2013-02-22 23:49:39 -0800

    more specs for ItemView

--- public/specs/the_spec.js
+++ public/specs/the_spec.js
@@ -1,11 +1,29 @@
 describe('Congo.ItemView', function(){
+  var view;
+
+  beforeEach( function(){
+    var spyModel = {
+      destroy: sinon.spy()
+    };
+    view = new Congo.ItemView({model:spyModel});
+  });
+
   it('throws a confirm dialog', function(){
     var confirmSpy = window.confirm = sinon.spy();
-
-    view = new Congo.ItemView()
     view.remove();
-
     expect(confirmSpy).toHaveBeenCalledWith('Delete this? You sure?');
+  });
+
+  it("doesn't delete the model if the deletion isn't confirmed", function(){
+    window.confirm = function(){ return false; }
+    view.remove();
+    expect( view.model.destroy ).not.toHaveBeenCalled();
+  });
+
+  it("deletes the model if the deletion isn't confirmed", function(){
+    window.confirm = function(){ return true; }
+    view.remove();
+    expect( view.model.destroy ).toHaveBeenCalled();
   });
 });
 
